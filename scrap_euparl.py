05d4d20644e6082cb465caf1cdea44a0afe5753f
Mathieu <albatros69+github@gmail.com>
2019-10-20 12:17:50 +0200

    Black...

--- scrap_euparl.py
+++ scrap_euparl.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from datetime import datetime, timedelta
 from hashlib import md5
@@ -14,24 +13,32 @@ from icalendar import Calendar, Event
 
 
 def extract_date(entity):
-    date = entity.find('div', class_='day').span
-    return datetime.strptime(date['content'], '%Y-%m-%dT%H:%M:%S%z')
+    date = entity.find("div", class_="day").span
+    return datetime.strptime(date["content"], "%Y-%m-%dT%H:%M:%S%z")
 
 
 def scrape_url(url):
     page = get(url)
 
-    result = [ ]
-    for div in BeautifulSoup(page.text, 'lxml').find_all('div', class_='view-events-by-day'):
-        for row in div.find_all('div', class_='views-row'):
-            location = ''.join(row.find('div', class_='views-field-field-field-nr-event-location').stripped_strings)
-            if 'Strasbourg' in location:
-                title = row.find('div', class_='views-field-title-field')
+    result = []
+    for div in BeautifulSoup(page.text, "lxml").find_all(
+        "div", class_="view-events-by-day"
+    ):
+        for row in div.find_all("div", class_="views-row"):
+            location = "".join(
+                row.find(
+                    "div", class_="views-field-field-field-nr-event-location"
+                ).stripped_strings
+            )
+            if "Strasbourg" in location:
+                title = row.find("div", class_="views-field-title-field")
                 tmp = {
-                    'date-start': extract_date(row.find('div', class_='event-date-start')), 
-                    'location':   location,
-                    'title':      ''.join(title.stripped_strings),
-                    'url':        title.a['href'],
+                    "date-start": extract_date(
+                        row.find("div", class_="event-date-start")
+                    ),
+                    "location": location,
+                    "title": "".join(title.stripped_strings),
+                    "url": title.a["href"],
                 }
                 result.append(tmp)
 
@@ -40,22 +47,24 @@ def scrape_url(url):
 
 def create_event(d):
     event = Event()
-    event.add('summary', d['title'])
-    event.add('dtstart', d['date-start'])
-    event.add('dtend',   d['date-start']+timedelta(days=3))
-    event.add('location', d['location'])
-    event.add('uid', '%s@europa.eu' % (md5(d['url'].encode('ascii')).hexdigest(), ))
-    event.add('description', 'https://europa.eu%s' % (d['url'],) )
+    event.add("summary", d["title"])
+    event.add("dtstart", d["date-start"])
+    event.add("dtend", d["date-start"] + timedelta(days=3))
+    event.add("location", d["location"])
+    event.add("uid", "%s@europa.eu" % (md5(d["url"].encode("ascii")).hexdigest(),))
+    event.add("description", "https://europa.eu%s" % (d["url"],))
 
     return event
 
 
 cal = Calendar()
-cal.add('prodid', '-//Calendrier Parlement Européen//europa.eu//')
-cal.add('version', '2.0')
+cal.add("prodid", "-//Calendrier Parlement Européen//europa.eu//")
+cal.add("version", "2.0")
 
-for d in scrape_url('https://europa.eu/newsroom/events/year_fr?field_nr_events_by_topic_tid=All&field_nr_event_source_tid=51&field_nr_event_type_tid=All'):
+for d in scrape_url(
+    "https://europa.eu/newsroom/events/year_fr?field_nr_events_by_topic_tid=All&field_nr_event_source_tid=51&field_nr_event_type_tid=All"
+):
     cal.add_component(create_event(d))
 
-with open('cal_euparl.ics', 'w') as f:
-   f.write(cal.to_ical().decode('utf-8'))
+with open("cal_euparl.ics", "w") as f:
+    f.write(cal.to_ical().decode("utf-8"))
